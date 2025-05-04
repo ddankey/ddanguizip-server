5b3bcf289b167d4c8014e328afffe80a1becf022
sycuuui <102959791+sycuuui@users.noreply.github.com>
2025-05-05 03:29:05 +0900

    #7 feat: 잘못된 code 요청 시 오류 발생ㅇ

--- src/main/java/com/ddanguizip/server/domain/map/service/MapService.java
+++ src/main/java/com/ddanguizip/server/domain/map/service/MapService.java
@@ -4,6 +4,7 @@ import com.ddanguizip.server.domain.location.entity.Location;
 import com.ddanguizip.server.domain.location.repository.LocationRepository;
 import com.ddanguizip.server.domain.map.dto.reponse.RiskAreaDetailList;
 import com.ddanguizip.server.domain.map.dto.reponse.RiskAreaListRes;
+import com.ddanguizip.server.domain.map.validator.MapValidator;
 import com.ddanguizip.server.domain.publicData.entity.SelectedRiskArea;
 import com.ddanguizip.server.domain.publicData.repository.SelectedRiskAreaRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,9 +21,13 @@ public class MapService {
     private final LocationRepository locationRepository;
     private final SelectedRiskAreaRepository selectedRiskAreaRepository;
 
+    private final MapValidator mapValidator;
+
     public RiskAreaListRes findRiskAreaList(String code, Pageable pageable) {
         //code를 통해서 location 찾기
         Location location = locationRepository.findLocationByCode(code);
+        mapValidator.validateLoaction(location);
+
         //location 통해서 데이터 추출
         Page<SelectedRiskArea> riskAreaPage = selectedRiskAreaRepository.searchList(location, pageable);
         List<RiskAreaDetailList> list = new ArrayList<>();
